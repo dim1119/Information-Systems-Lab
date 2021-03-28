10d43489ec7b909674942c6aab07c38a9390deb4
Dimitris Lazarakis <dlazara@gmail.com>
2021-03-28 22:47:09 +0300

    Lab 1 assignment

---
+++ lab1/assignments/ask2.py
@@ -0,0 +1,13 @@
+my_dict = {"a": 10, "b": 20, "c": 30}
+
+
+print("Sum is {}".format(sum(my_dict.values())))
+
+my_list = [my_dict]
+
+
+def count_list(mylist):
+    print("Mean AVG {}".format(sum(mylist[0].values())/len(mylist[0])))
+
+
+count_list(my_list)
